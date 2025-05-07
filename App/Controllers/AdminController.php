2f0498c275da6e712b9a2bf5592757ec96be2c1f
PhuQui265 <phuquin46@gmail.com>
2025-05-07 10:20:53 +0700

    change admin feature

---
+++ App/Controllers/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/../Model/AdminModel.php';
+
+class AdminController
+{
+    public function index()
+    {
+        include __DIR__ . '/../Views/Admin/index.php';
+    }
+    
+} 
